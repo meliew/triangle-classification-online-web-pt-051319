b27e6c423c658f5e2d08e2da1041ab89377bb492
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-15 05:28:51 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,12 +9,9 @@ class Triangle
   end
 
   def kind
-    if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2
+    if ((s1 * s2 * s3) == 0) || ((s1 + s2) <= s3) || ((s2 + s3) <= s1) || ((s3 + s1) <= s2)
 
-        raise TriangleError
-      #   puts error.message
-      # end
-
+      raise TriangleError
 
     elsif s1 == s2 && s1 == s3
       self.kind = :equilateral
@@ -26,50 +23,9 @@ class Triangle
       self.kind = :scalene
     end
   end
-end
 
+  class TriangleError < StandardError
 
-class TriangleError < StandardError
-  def message
-    puts "not a triangle"
   end
 
 end
-
-
-
-
-#   def initialize(side1, side2, side3)
-#     @side1 = side1
-#     @side2 = side2
-#     @side3 = side3
-#   end
-#
-#
-#   def kind
-#   # if (@side1 * @side2 * @side3) == 0 || (@side1 + @side2) <= @side3 || (@side2 + @side3) <= @side1
-#     if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
-#       raise TriangleError
-#
-#     elsif (@side1+@side2 <= @side3) || (@side1+@side3 <= @side2) || (@side2+@side3 <= @side1)
-#       raise TriangleError
-#     elsif
-#       @side1 == @side2 && @side1 == @side3
-#       :equilateral
-#     elsif
-#       @side1 == @side2
-#       :isosceles
-#     elsif
-#       @side2 == @side3
-#        :isosceles
-#      elsif
-#        @side1 == @side3
-#         :isosceles
-#      elsif
-#       @side1 != @side2
-#       @side2 != @side3
-#       @side1 != @side4
-#       :scalene
-#       end
-#     end
-# end
