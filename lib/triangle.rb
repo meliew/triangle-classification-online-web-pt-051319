299ab661726a82d3bd2523b7d6f366019412e485
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-15 03:13:48 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
 
   def kind
     if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2
-      begin
+    
         raise TriangleError
         puts error.message
       end
